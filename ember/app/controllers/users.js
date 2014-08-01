495758ba28ad35cb62ebe2611a780cc650edbb58
Blake Erickson <o.blakeerickson@gmail.com>
2014-08-01 06:18:33 -0600

    got action in controller to be called

--- ember/app/controllers/users.js
+++ ember/app/controllers/users.js
@@ -6,13 +6,15 @@ export default Ember.ArrayController.extend({
       var title = this.get('newTitle');
       if (!title.trim()) { return; }
 
+      // Commented out because I don't have the rails backend
+      //  setup yet to create a todo.
       // var todo = this.store.createRecord('todo', {
       //   title: title
       // });
 
       this.set('newTitle', '');
 
-      //todo.save();
+      // todo.save();
     }
   }
 });
